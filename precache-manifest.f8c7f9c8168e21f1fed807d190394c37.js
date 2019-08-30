201cfc5860c6065e7caca6b00351aa1ea493cf9e
franzcarl <franz@bisdahin.de>
2019-08-30 10:14:09 +0200

    Updates

---
+++ precache-manifest.f8c7f9c8168e21f1fed807d190394c37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8e84675c624768d412e6d52c45f20e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "473633d1443bf8bda096",
+    "url": "./static/js/2.82228227.chunk.js"
+  },
+  {
+    "revision": "a18959ca75e184f523a8",
+    "url": "./static/js/main.efcaa620.chunk.js"
+  },
+  {
+    "revision": "17625a14df96d807cd5f",
+    "url": "./static/js/runtime~main.b36dd38b.js"
+  }
+]);
